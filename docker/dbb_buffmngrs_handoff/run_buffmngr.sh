134c6b5b464827077fbcc02a0897cc303b4252d4
Mikolaj Kowalik <mxk@illinois.edu>
2020-02-18 14:19:14 -0600

    Include provenance information
    No provenance information were logged previously.  Modified the driver
    to start logging some rudimentary provenance information like the
    version of the LSST stack used to run the buffer manager.

--- docker/dbb_buffmngrs_handoff/run_buffmngr.sh
+++ docker/dbb_buffmngrs_handoff/run_buffmngr.sh
@@ -2,6 +2,21 @@
 #
 # run_buffmngr.sh: sets up and start the buffer manager
 
+exec &> /var/log/buffmngrs/handoff.log
+
 source /opt/lsst/software/stack/loadLSST.bash
-setup -r /opt/lsst/addons/dbb_buffmngrs_handoff
+setup --verbose --root /opt/lsst/addons/dbb_buffmngrs_handoff
+
+echo "--- Environemnt ---"
+eups list --setup
+echo
+
+echo "--- Configuration ---"
+cat $1
+echo
+
+echo "--- Runtime ---"
 transd.py -c $1
+echo
+
+exit 0
