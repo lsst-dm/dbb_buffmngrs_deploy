9427cd5954b0881d0c4cc7b2ca4fdbf43b4f6ea3
Mikolaj Kowalik <mxk@illinois.edu>
2020-05-11 09:56:29 -0500

    Streamline deployment with docker-compose
    Deploying DBB handoff managers for both Auxtel and Comcam using current
    approach was a bit tedious and error prone. Firstly, the  configuration
    file was a part of the docker image.  Thus the image needs rebuilding,
    twice, when any configuration change occurred.  Secondly, the scripts
    were coupled in subtle and obfuscated ways (e.g. changing a log name in
    one required manual updates in others).  To address these issues I made
    following changes:
        * The configuration file is no longer a part of an image. Now it is
          bind mounted to it.
        * Replaced custom scripts with a single docker-compose file which
          controls practically aspects of the deployment.

---
+++ docker/env.bash
@@ -0,0 +1,21 @@
+# Version of the LSST stack to use.
+export LSST_VER="d_latest"
+
+# Version of the DBB buffer manager to use.
+export MNGR_VER="1.0.0-rc1"
+
+# Label which will be applied to the resultant Docker image. 
+export TAG="${MNGR_VER}"
+
+# User which will run the image. User MUST exists!
+export USER="arc"
+
+# Derived or internal environmental variables used by docker-compose.
+#
+# WARNING
+# -------
+# Do NOT change unless you know what you're doing! Seriously.
+export USR_ID=`id -u ${USER}`
+export GRP_ID=`id -g ${USER}`
+export SRC_HOME=`eval "echo ~${USER}"`
+export TGT_HOME="/home/mngr"
