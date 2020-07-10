a77ead7f2d030446142afe02313a84bf9cd23dff
Mikolaj Kowalik <mxk@illinois.edu>
2020-07-10 08:48:59 -0500

    Bump software version in config file

--- handoff/env.bash
+++ handoff/env.bash
@@ -1,14 +1,14 @@
 # Version of the LSST stack to use.
-export LSST_VER="7-stack-lsst_distrib-w_2020_16"
+export LSST_VER="7-stack-lsst_distrib-w_2020_26"
 
 # Version of the DBB buffer manager to use.
-export MNGR_VER="1.0.0"
+export MNGR_VER="1.1.0"
 
 # Label which will be applied to the resultant Docker image. 
 export TAG="${MNGR_VER}-${LSST_VER##*-}"
 
 # User which will run the image. User MUST exist!
-export USER="arc"
+export USER="ARC"
 
 # Derived and internal environmental variables used by docker-compose.
 #
