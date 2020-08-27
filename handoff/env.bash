0084f670d572c7eed42d899bb5bec84bc9fc652d
Mikolaj Kowalik <mxk@illinois.edu>
2020-08-27 07:42:13 -0500

    Enable using bbcp
    Starting from version 1.2, DBB handoff buffer manager allows a user to
    specify the command which will be used to transfer files between the
    handoff and endpoint sites different from scp (e.g. bbcp).  However,
    bbcp is not included in CentOS package repositories.  Hence, I added it
    source and modified the Dockerfile to make it compile, build, and
    install it in the Docker image.  I also updated provided example
    configuration file to make it reflect new syntax.

--- handoff/env.bash
+++ handoff/env.bash
@@ -1,19 +1,20 @@
 # Version of the LSST stack to use.
-export LSST_VER="7-stack-lsst_distrib-w_2020_26"
+export LSST_VER="7-stack-lsst_distrib-w_2020_33"
 
 # Version of the DBB buffer manager to use.
-export MNGR_VER="1.1.0"
+export MNGR_VER="1.2.0-rc1"
 
 # Label which will be applied to the resultant Docker image. 
 export TAG="${MNGR_VER}-${LSST_VER##*-}"
 
 # User which will run the image. User MUST exist!
-export USER="ARC"
+export USER="arc"
 
 # Derived and internal environmental variables used by docker-compose.
 #
 # WARNING
 # -------
 # Do NOT change unless you know what you're doing! Seriously.
+export BBCP_VER="17.12.00.00.0"
 export USR_ID=`id -u ${USER}`
 export GRP_ID=`id -g ${USER}`
